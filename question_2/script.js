5ad4dcd9315841694be21c52b209ef66ad65a499
Arantis_jr <arantisjunior@gmail.com>
2025-04-03 13:26:39 +0100

    fix:completed question 2

--- question_2/script.js
+++ question_2/script.js
@@ -4,7 +4,7 @@ DisplayTime = document.getElementById("display");
     const date =  new Date()
     let hour = date.getHours();
     let minute = date.getMinutes()
-     let day = date.getDate()        //toLocaleString("default",{day: "short"});
+     let day = date.toLocaleString('en-US',{weekday: 'short'})        //toLocaleString("default",{day: "short"});
     let month = date.toLocaleString("default", {month: "short"});
 
 DisplayTime.innerHTML = `${month} ${day}  ${hour}:${minute}`
